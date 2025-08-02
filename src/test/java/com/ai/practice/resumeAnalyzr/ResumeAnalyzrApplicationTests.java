46efee8837aed20bd283813aed347827916ca8bc
Abhilash Guha <abhilashguha91@gmail.com>
2025-08-02 20:42:30 +0530

    Basic Framework for the Project

---
+++ src/test/java/com/ai/practice/resumeAnalyzr/ResumeAnalyzrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.practice.resumeAnalyzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
